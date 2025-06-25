87b704f801a4db9d96defa60182e4a070ac8cf30
darkhell1 <orcunkeskin0@gmail.com>
2025-06-25 14:25:52 +0300

    Add files via upload

---
+++ demo verbisai/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: 40 }}>
+      <h1>Verbis AI</h1>
+      <p>Yapay zeka destekli asistanınıza hoş geldiniz.</p>
+    </main>
+  );
+}
